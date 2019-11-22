4e76811d0057698713c2e952f06d896d8f6773ef
Dmitry Povolotsky <krizai@readdle.com>
2019-11-22 09:41:44 +0100

    SPX-8026 - Unblock Cmd+Enter hotkey
    Cmd+Enter is a hotkey to send email, so we shouldn't block it.

--- dist/quill.core.js
+++ dist/quill.core.js
@@ -4657,7 +4657,7 @@ var Keyboard = function (_Module) {
       }
     });
     _this.addBinding({ key: Keyboard.keys.ENTER, shiftKey: null }, handleEnter);
-    _this.addBinding({ key: Keyboard.keys.ENTER, metaKey: null, ctrlKey: null, altKey: null }, function () {});
+    _this.addBinding({ key: Keyboard.keys.ENTER, metaKey: false, ctrlKey: null, altKey: null }, function () {});
     if (/Firefox/i.test(navigator.userAgent)) {
       // Need to handle delete and backspace for Firefox in the general case #1171
       _this.addBinding({ key: Keyboard.keys.BACKSPACE }, { collapsed: true }, handleBackspace);
